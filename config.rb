a90e24024acfa426c9c0d514aa35805988756a74
Robert Fletcher <lobatifricha@gmail.com>
2026-06-03 16:33:14 -0700

    Deps: upgrade Ruby to 4.0.5

--- config.rb
+++ config.rb
@@ -1,3 +1,21 @@
+require 'rouge'
+require 'rouge/plugins/redcarpet'
+require 'middleman-core/renderers/redcarpet'
+
+# Subclasses Middleman's renderer so the image/link helpers keep working, and
+# mixes in SmartyPants . The Github theme CSS (syntax_highlighting.css.erb) is
+# scoped to `.highlight`, which is the wrapper we emit below.
+class RougeRedcarpetHTML < Middleman::Renderers::MiddlemanRedcarpetHTML
+  include Redcarpet::Render::SmartyPants
+  include Rouge::Plugins::Redcarpet
+
+  def block_code(code, language)
+    lexer = Rouge::Lexer.find_fancy(language, code) || Rouge::Lexers::PlainText
+    table = Rouge::Formatters::HTMLTable.new(Rouge::Formatters::HTML.new)
+    %(<div class="highlight">#{table.format(lexer.lex(code))}</div>)
+  end
+end
+
 activate :blog do |blog|
   blog.paginate = true
   blog.sources = 'articles/{year}-{month}-{day}-{title}.html'
@@ -6,10 +24,9 @@ activate :blog do |blog|
 end
 
 activate :asset_hash
-activate :syntax, line_numbers: true
 set :markdown_engine, :redcarpet
-set :markdown, fenced_code_blocks: true,
-               smartypants: true,
+set :markdown, renderer: RougeRedcarpetHTML,
+               fenced_code_blocks: true,
                space_after_headers: true,
                no_intra_emphasis: true,
                tables: true,
